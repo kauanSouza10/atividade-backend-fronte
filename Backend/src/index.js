ff8028f208ca20762b4fd99af1063a3462008198
Kauan Ferreira <kauan.souza@eaportal.org>
2025-11-07 09:54:03 -0300

    FIM

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,15 +6,28 @@ const app = express()
 const port = 3333
 
 app.use(cors())
+app.use(express.json())
+//GET, POST, PUT, PATCH, DELETE
 app.get("/", (request, response) => {
     response.json(people)
 })
 
 app.post("/cadastrar", (request, response) => {
-    console.log(request.body)
+    const { name, email, age, password } = request.body.user
+
+    console.log(`
+        Nome: ${name},
+        E-mail: ${email},
+        Idade: ${age},
+        Senha: ${password}
+    `)
+
+    //guardar usuario no banco de dados
+
+    response.status(201).json({ message: "Usuário cadastrado com sucesso!" })
 })
 
+//Deixar o servidor online
 app.listen(port, () => {
     console.log(`Servidor rodando na porta: ${port}`)
-    
 })
